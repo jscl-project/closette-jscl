458daf044340a1408a94155522de002626a20d1a
vlad-km <gm5461@gmail.com>
2018-05-19 15:09:51 +0300

    add functions

--- utils.lisp
+++ utils.lisp
@@ -15,9 +15,54 @@
 
 
 
-
-
 (in-package :closette)
+
+;;; kludge for (defun (setf name) ...) syntax
+(defun setf-function-symbol (spec)
+    (if (consp spec)
+        (let ((print-name (write-to-string spec)))
+            (intern print-name
+                    (symbol-package (cadr spec))))
+        spec))
+
+(defmacro defun* (name forms &rest body)
+    (cond ((symbolp name)
+           `(defun ,name ,forms ,@body))
+          ((and (consp name) (eq (car name) 'setf))
+           (let ((sfn (setf-function-symbol name)))
+               `(progn
+                    (defun ,sfn ,forms ,@body)
+                    (defsetf ,(cadr name) ,sfn))))
+          (t (error "defun* ~a unknow function specifier" name))))
+
+
+;;; (setf (cadr lst) something)
+(defun* (setf cadr) (lst value) (rplaca (cdr lst) value))
+
+
+;;; kludge to rotatef
+(defmacro psetf (&rest assignments)
+    (when assignments
+        (when (null (cdr assignments))
+            (error "PSETF odd number of arguments"))
+        (let ((value (gensym)))
+            `(let ((,value ,(cadr assignments)))
+                 (psetf ,@(cddr assignments))
+                 (setf ,(car assignments) ,value)
+                 nil))))
+
+;;; dumb rotatef
+(defmacro rotatef (&rest places)
+    (when places
+        (let ((args '()))
+            (dolist (x places)
+                (push x args)
+                (push x args))
+            (push (first places) args)
+            (setf args (nreverse args))
+            (setf (car args) 'psetf)
+            `(progn ,args 'nil))))
+
 
 
 (defmacro print-unreadable-object((object stream &key type identity) &body body)
@@ -35,23 +80,24 @@
          nil))
 
 ;;;
-(defun get-properties (plist indicator-list)
+(defun get-properties (plist indicator)
     (do ((it  plist (cddr it)))
         ((null it)
          (values nil nil nil))
-        (when (member (car it) indicator-list)
+        (when (member (car it) indicator)
             (return (values (first it) (second it) it)))))
 
 
 
 ;;; push-on-end is like push except it uses the other end:
-
 (defmacro push-on-end (value location)
     `(setf ,location (nconc ,location (list ,value))))
 
 ;;; (setf getf*) is like (setf getf) except that it always changes the list,
 ;;;              which must be non-nil.
 ;;; defsetf getf*
+
+#|
 (defun setf-getf* (plist key new-value)
     (block body
         (do ((x plist (cddr x)))
@@ -64,6 +110,20 @@
         new-value))
 
 (defsetf getf* setf-getf*)
+|#
+
+(defun* (setf getf*) (plist key new-value)
+    (block body
+        (do ((x plist (cddr x)))
+            ((null x))
+            (when (eq (car x) key)
+                (setf (car (cdr x)) new-value)
+                (return-from body new-value)))
+        (push-on-end key plist)
+        (push-on-end new-value plist)
+        new-value))
+
+
 
 ;;; mapappend is like mapcar except that the results are appended together:
 (defun mapappend (fun &rest args)
@@ -81,35 +141,34 @@
 
 
 ;;; set-difference
-(defun set-difference (list-1 list-2 &key key (test #'equal))
-    (cond (list-2
+(defun set-difference (list1 list2 &key key (test #'eq))
+    (cond (list2
            (let ((result '()))
-               (dolist (e list-1)
-                   (when (not (member e list-2 :key key :test test))
-                       (push e result)))
+               (dolist (it list1)
+                   (when (not (member e list2 :key key :test test))
+                       (push it result)))
                result))
-          (t
-           list-1)))
+          (t list1)))
 
 ;;; union
-(defun union (list-1 list-2 &key key (test #'equal))
-    (cond ((and list-1 list-2)
-           (let ((result list-2))
-               (dolist (e list-1)
-                   (when (not (member e list-2 :key key :test test))
-                       (push e result)))
+(defun union (list1 list2 &key key (test #'eq))
+    (cond ((and list1 list2)
+           (let ((result list2))
+               (dolist (it list1)
+                   (when (not (member it list2 :key key :test test))
+                       (push it result)))
                result))
-          (list-1)
-          (list-2)))
+          (list1)
+          (list2)))
 
 
 
 ;;; intersection
-(defun intersection (list-1 list-2 &key key (test #'eql))
+(defun intersection (list1 list2 &key key (test #'eql))
     (let ((result '()))
-        (dolist (e list-1)
-            (when (member e list-2 :key key :test test )
-                (push e result)))
+        (dolist (it list1)
+            (when (member it list2 :key key :test test )
+                (push it result)))
         result))
 
 
@@ -143,23 +202,19 @@
 
 ;;; sort
 (defun sort1 (seq fn key)
-    (cond ((endp seq)
-           '())
-          (t
-           (do* ((ipos seq (cdr ipos))
-                 (imin ipos ipos))
-                ((null ipos)
-                 seq)
-               (do ((i (cdr ipos) (cdr i)))
-                   ((null i))
-                   (when (funcall fn (funcall key (car i)) (funcall key (car imin)))
-                       (setf imin i)))
-               (when (not (eq imin ipos))
-                   ;; Swap
-                   (let ((old-ipos (car ipos))
-                         (old-imin (car imin)))
-                       (setf (car ipos) old-imin
-                             (car imin) old-ipos)))))))
+    (cond ((endp seq) '())
+          (t (do* ((ipos seq (cdr ipos))
+                   (imin ipos ipos))
+                  ((null ipos) seq)
+                 (do ((i (cdr ipos) (cdr i)))
+                     ((null i))
+                     (when (funcall fn (funcall key (car i)) (funcall key (car imin)))
+                         (setf imin i)))
+                 (when (not (eq imin ipos))
+                     (let ((old-ipos (car ipos))
+                           (old-imin (car imin)))
+                         (setf (car ipos) old-imin
+                               (car imin) old-ipos)))))))
 
 (defun sort (seq fn &key key)
     (unless key (setf key 'identity))
