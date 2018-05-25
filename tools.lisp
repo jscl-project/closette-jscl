1ae2705c786cf10e5b8d2ee479ddbc285c5815db
vlad-km <gm5461@gmail.com>
2018-05-25 12:43:34 +0300

    rename package

--- tools.lisp
+++ tools.lisp
@@ -6,26 +6,26 @@
 (in-package :cl-user)
 
 (defun subclasses* (class)
-    (closette::remove-duplicates
+    (clos::remove-duplicates
      (cons class
-           (closette::mapappend #'subclasses*
-                                (closette::class-direct-subclasses class)))))
+           (clos::mapappend #'subclasses*
+                            (clos::class-direct-subclasses class)))))
 
 (export '(subclasses))
 (defun subclasses (class &optional names)
     (let ((result (remove class (subclasses* class))))
-        (if names (mapcar #'closette::class-name result)
+        (if names (mapcar #'clos::class-name result)
             result)))
 
 (export '(in-order-p))
 (defun in-order-p (c1 c2)
     (flet ((in-order-at-subclass-p (sub)
-               (let ((cpl (closette::class-precedence-list sub)))
+               (let ((cpl (clos::class-precedence-list sub)))
                    (not (null (member c2 (cdr (member c1 cpl))))))))
         (or (eq c1 c2)
             (every #'in-order-at-subclass-p
-                   (closette::intersection (subclasses* c1)
-                                           (subclasses* c2))))))
+                   (clos::intersection (subclasses* c1)
+                                       (subclasses* c2))))))
 
 ;;; display defclass
 
@@ -45,34 +45,34 @@
 
 (defun generate-defclass (class)
     `(,(class-name class)
-      ,(mapcar #'class-name (cdr (closette::class-precedence-list class)))
+      ,(mapcar #'class-name (cdr (clos::class-precedence-list class)))
       ,(mapcar #'(lambda (slot)
                      (generate-inherited-slot-specification class slot))
-               (closette::class-slots class))))
+               (clos::class-slots class))))
 
 (defun generate-slot-specification (slot)
-    `(,(closette::slot-definition-name slot)
-      ,@(when (closette::slot-definition-initfunction slot)
-            `(:initform ,(closette::slot-definition-initform slot)))
-      ,@(when (closette::slot-definition-initargs slot)
-            (closette::mapappend #'(lambda (initarg) `(:initarg ,initarg))
-                                 (closette::slot-definition-initargs slot)))
-      ,@(unless (eq (closette::slot-definition-allocation slot) ':instance)
-            `(:allocation ,(closette::slot-definition-allocation slot)))
-      ,@(when (closette::slot-definition-readers slot)
-            (closette::mapappend #'(lambda (reader) `(:reader ,reader))
-                                 (closette::slot-definition-readers slot)))
-      ,@(when (closette::slot-definition-writers slot)
-            (closette::mapappend #'(lambda (writer) `(:writer ,writer))
-                                 (closette::slot-definition-writers slot)))))
+    `(,(clos::slot-definition-name slot)
+      ,@(when (clos::slot-definition-initfunction slot)
+            `(:initform ,(clos::slot-definition-initform slot)))
+      ,@(when (clos::slot-definition-initargs slot)
+            (clos::mapappend #'(lambda (initarg) `(:initarg ,initarg))
+                             (clos::slot-definition-initargs slot)))
+      ,@(unless (eq (clos::slot-definition-allocation slot) ':instance)
+            `(:allocation ,(clos::slot-definition-allocation slot)))
+      ,@(when (clos::slot-definition-readers slot)
+            (clos::mapappend #'(lambda (reader) `(:reader ,reader))
+                             (clos::slot-definition-readers slot)))
+      ,@(when (clos::slot-definition-writers slot)
+            (clos::mapappend #'(lambda (writer) `(:writer ,writer))
+                             (clos::slot-definition-writers slot)))))
 
 (defun generate-inherited-slot-specification (class slot)
     (let* ((source-class
              (find-if #'(lambda (superclass)
-                            (find (closette::slot-definition-name slot)
-                                  (closette::class-direct-slots superclass)
-                                  :key #'closette::slot-definition-name))
-                      (closette::class-precedence-list class)))
+                            (find (clos::slot-definition-name slot)
+                                  (clos::class-direct-slots superclass)
+                                  :key #'clos::slot-definition-name))
+                      (clos::class-precedence-list class)))
            (generated-slot-spec
              (generate-slot-specification slot)))
         (if (eq source-class class)
@@ -80,16 +80,14 @@
             (append generated-slot-spec
                     `(:inherited-from ,(class-name source-class))))))
 
-
-
 ;;; display generate-defgeneric
 (defun generate-defgeneric (gf)
-    `(defgeneric ,(closette::generic-function-name gf)
-         ,(closette::generic-function-lambda-list gf)))
+    `(defgeneric ,(clos::generic-function-name gf)
+         ,(clos::generic-function-lambda-list gf)))
 
 (export '(display-defgeneric))
 (defun display-defgeneric (arg)
-    (let ((gf (if (symbolp arg) (closette::find-generic-function arg) arg)))
+    (let ((gf (if (symbolp arg) (clos::find-generic-function arg) arg)))
         (print (generate-defgeneric gf))
         (terpri)
         (values)))
@@ -97,14 +95,14 @@
 
 ;;; display-generic-function
 (defun generate-defmethod (method &key show-body)
-    `(defmethod ,(closette::generic-function-name (closette::method-generic-function method))
-         ,@(closette::method-qualifiers method)
+    `(defmethod ,(clos::generic-function-name (clos::method-generic-function method))
+         ,@(clos::method-qualifiers method)
          ,(generate-specialized-arglist method)
-         ,@(when show-body (list (closette::method-body method)))))
+         ,@(when show-body (list (clos::method-body method)))))
 
 (defun generate-specialized-arglist (method)
-    (let* ((specializers (closette::method-specializers method))
-           (lambda-list (closette::method-lambda-list method))
+    (let* ((specializers (clos::method-specializers method))
+           (lambda-list (clos::method-lambda-list method))
            (number-required (length specializers)))
         (append (mapcar #'(lambda (arg class)
                               (if (eq class (find-class 't))
@@ -128,7 +126,7 @@
 (export '(display-generic-function))
 (defun display-generic-function (gf-name &key show-body)
     (display-defgeneric gf-name)
-    (dolist (method (closette::generic-function-methods (closette::find-generic-function gf-name)))
+    (dolist (method (clos::generic-function-methods (clos::find-generic-function gf-name)))
         (print (generate-defmethod method :show-body show-body)))
     (values))
 
@@ -137,35 +135,35 @@
 (defun all-generic-functions (&key names)
     (let ((result))
         (setq result
-              (closette::remove-duplicates
-               (closette::mapappend #'class-direct-generic-functions
-                                    (subclasses* (find-class 't)))))
+              (clos::remove-duplicates
+               (clos::mapappend #'class-direct-generic-functions
+                                (subclasses* (find-class 't)))))
         (if names
-            (mapcar #'closette::generic-function-name result)
+            (mapcar #'clos::generic-function-name result)
             result)))
 
 
 (defun class-direct-generic-functions (class)
-    (closette::remove-duplicates
-     (mapcar #'closette::method-generic-function
-             (closette::class-direct-methods class))))
+    (clos::remove-duplicates
+     (mapcar #'clos::method-generic-function
+             (clos::class-direct-methods class))))
 
 
 ;;; relevant generic function
 (defun reader-method-p (method)
-    (let ((specializers (closette::method-specializers method)))
+    (let ((specializers (clos::method-specializers method)))
         (and (= (length specializers) 1)
-             (member (closette::generic-function-name (closette::method-generic-function method))
-                     (closette::mapappend #'closette::slot-definition-readers
-                                          (closette::class-direct-slots (car specializers)))
+             (member (clos::generic-function-name (clos::method-generic-function method))
+                     (clos::mapappend #'clos::slot-definition-readers
+                                      (clos::class-direct-slots (car specializers)))
                      :test #'equal))))
 
 (defun writer-method-p (method)
-    (let ((specializers (closette::method-specializers method)))
+    (let ((specializers (clos::method-specializers method)))
         (and (= (length specializers) 2)
-             (member (closette::generic-function-name (closette::method-generic-function method))
-                     (closette::mapappend #'closette::slot-definition-writers
-                                          (closette::class-direct-slots (cadr specializers)))
+             (member (clos::generic-function-name (clos::method-generic-function method))
+                     (clos::mapappend #'clos::slot-definition-writers
+                                      (clos::class-direct-slots (cadr specializers)))
                      :test #'equal))))
 
 (defun relevant-generic-functions (class ceiling &key elide-accessors-p (names nil))
@@ -173,18 +171,18 @@
           (fceiling (if (symbolp ceiling) (find-class ceiling) ceiling))
           (result))
         (setq result
-              (closette::remove-duplicates
-               (mapcar #'closette::method-generic-function
+              (clos::remove-duplicates
+               (mapcar #'clos::method-generic-function
                        (remove-if
                         #'(lambda (m)
                               (and elide-accessors-p
                                    (or (reader-method-p m)
                                        (writer-method-p m))))
-                        (closette::mapappend #'closette::class-direct-methods
-                                             (closette::set-difference (closette::class-precedence-list fclass)
-                                                                       (closette::class-precedence-list fceiling)))))))
+                        (clos::mapappend #'clos::class-direct-methods
+                                         (clos::set-difference (clos::class-precedence-list fclass)
+                                                               (clos::class-precedence-list fceiling)))))))
         (if names
-            (mapcar #'closette::generic-function-name result)
+            (mapcar #'clos::generic-function-name result)
             result)))
 
 ;;; EOF
