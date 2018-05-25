d735e24bc353ffd3356c16b8a3afd1ae53fbe7d0
vlad-km <gm5461@gmail.com>
2018-05-25 12:52:23 +0300

    fix

--- closette-sdf.lisp
+++ closette-sdf.lisp
@@ -3,14 +3,14 @@
 ;;; System CLOSETTE definition file
 ;;; for compilation with LORES:QLOAD
 ;;;
+
 (lores:defsys :closette
     :path "git/jscl-closette"
-    :components
-    ((:file "package")
-     (:file "utils")
-     (:file "closette")
-     (:file "closette-boot")
-     (:file "closette-final")
-     (:file "tools")))
+    :components ((:file "package")
+                 (:file "utils")
+                 (:file "closette")
+                 (:file "closette-boot")
+                 (:file "closette-final")
+                 (:file "tools")))
 
 ;;; eof
