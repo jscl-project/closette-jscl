26b622a48b18d11b36b6a3f0344ae6d1890036a3
vlad-km <gm5461@gmail.com>
2018-05-27 07:38:03 +0300

    add alive

--- banner.lisp
+++ banner.lisp
@@ -1,32 +1,136 @@
 ;;; -*- mode:lisp; coding:utf-8 -*-
 
-(defpackage :clos.banner
-  (:use :cl))
-
 
 (in-package :clos.banner)
 
-(defparameter *ban*
-  '(
-    " "
-    "   ____   _        ___    ____ "
-    "  / ___| | |      / _ \\  / ___| "
-    " | |     | |     | | | | \\___ \\"
-    " | |___  | |___  | |_| |  ___) | "
-    "  \\____| |_____|  \\___/  |____/"
-    " "
-    " "
-    ))
+(defvar *clos-alive-time* (#j:Date:now))
+
+
+(defun compute-alive ()
+    (let* ((ss0 (floor (/ (- (#j:Date:now) clos-banner::*clos-alive-time*) 1000)))
+           (hms (/ ss0 3600))
+           (hh (floor hms))
+           (rmm  (* (- hms  hh) 60))
+           (mm (floor rmm))
+           (ss (#j:Math:ceil (* (- rmm mm) 60))))
+        (values hh mm ss)))
+
+(defun make-lisp-info ()
+    (format nil "~a ~a ~a"
+            (lisp-implementation-type)
+            (lisp-implementation-version)
+            (jscl::compilation-notice)))
+
+
+(defun date/component (obj key)
+    (funcall ((jscl::oget obj key "bind") obj)))
+
+
+(defun make-compile-date ()
+    (let ((dt (jscl::make-new #j:Date)))
+        (jscl::concat ""
+                      (date/component dt "getFullYear") "/"
+                      (1+ (date/component dt "getMonth")) "/"
+                      (date/component dt "getDate") " "
+                      (date/component dt "getHours") ":"
+                      (date/component dt "getMinutes") ":"
+                      (date/component dt "getSeconds") " ")))
+
+
+(defparameter clos-compile-info
+  (list
+   "The JSCL port CLOSETTE system. Was compile the Lores from Moren environment"
+   (format nil "~a" (make-lisp-info))
+   (format nil "CLOS beta.02.0 built on ~a" (make-compile-date))))
+
+
+(defun banner-info (banner)
+    (let ((height (length banner))
+          (width)
+          (wl))
+
+        (setq width
+              (apply 'min (dolist (it banner wl)
+                              (push (length it) wl))))
+        (values height width) ))
+
+(defun banner-from-lib (number)
+    (let* ((num (if number number (random (length *banlib*))))
+           (banner (copy-list (nth num *banlib*))))
+        (multiple-value-bind (height width) (banner-info banner)
+            (list height width banner))))
 
 
 
-(defun draw (&optional (banner *ban*))
-    (dolist (it banner)
-        (#j:jqconsole:Write it "jqconsole-output" nil)
-        (terpri)))
+(defun printer (info)
+    (#j:jqconsole:Write info "jqconsole-output" nil)
+    (terpri))
+
+
+;;; comile info
+(defun compile-info (&optional number)
+    (dolist (it (glue-compile (banner-from-lib number)))
+        (printer it))
+    (terpri)
+    (terpri)
+    (values))
+
+
+(defparameter clos-compile-info
+  (list
+   "The JSCL port CLOSETTE system. Was compile the Lores from Moren environment"
+   (format nil "~a" (make-lisp-info))
+   (format nil "CLOS beta.02.0 built on ~a" (make-compile-date))))
+
+
+(defun glue-compile (ban-elt &optional (glue clos-compile-info))
+    (let ((glue-line '(0 1 2))
+          (pos 0)
+          (pad))
+        (DESTRUCTURING-BIND (height width banner) ban-elt
+            (cond ((>= height 6) (setq glue-line '(1 3 4)))
+                  ((= height 5) (setq glue-line '(1 2 3)))
+                  ((= height 4) (setq glue-line '(0 2 3))))
+            (setq pad (make-string 3))
+            (dotimes (idx (length glue))
+                (setq pos (nth idx glue-line))
+                (setf (nth pos banner)
+                      (jscl::concat (nth pos banner)
+                                    pad
+                                    (nth idx glue))))
+            banner)))
+
 
 (dotimes (i 6) (terpri))
-(draw)
+(compile-info)
+
+
+;;; alive info
+
+(export '(alive))
+
+(defun alive (&optional number)
+    (terpri)
+    (dolist (it (glue-alive (banner-from-lib number)))
+        (printer it))
+    (terpri)
+    (terpri)
+    (values))
+
+(defun glue-alive (banner)
+    (glue-compile banner
+                  (list
+                   (multiple-value-bind (hh mm ss) (compute-alive)
+                       (format nil "Alive time ~a.h ~a.m ~a.s" hh mm ss))
+                   (format nil "Classes ~a " (class-alive-info))
+                   (format nil "Generic functions ~a" (generic-alive-info)))))
+
+(defun class-alive-info ()
+    (hash-table-count clos::*class-table*))
+
+(defun generic-alive-info ()
+    (hash-table-count clos::*generic-function-table*))
+
 
 (in-package :cl-user)
 
